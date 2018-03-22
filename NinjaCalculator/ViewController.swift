a591b124b3960196ff80d83aa837db103795d58e
sadikuls <sadikul.bari2209@gmail.com>
2018-03-22 16:30:40 +0600

    Completed : completed simple calculator as learning aspect

--- NinjaCalculator/ViewController.swift
+++ NinjaCalculator/ViewController.swift
@@ -11,7 +11,24 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblOutputTxgt: UILabel!
+    
+    enum  Operation: String {
+        case Divide = "/"
+        case Add = "+"
+        case Subtract = "-"
+        case Multiply = "*"
+        case Equal = "="
+        case Empty = "Empty"
+    }
     var btnSound: AVAudioPlayer!
+    var currentOperation = Operation.Empty
+    
+    var runningNumebr = ""
+    var leftValueStar = ""
+    var rightValStar = ""
+    var resultValStar = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,6 +45,36 @@ class ViewController: UIViewController {
 
     @IBAction func numberPressed(sender: UIButton){
         playSound()
+        runningNumebr += "\(sender.tag)"
+        lblOutputTxgt.text = runningNumebr
+        
+        
+    }
+    
+    @IBAction func devideButtonPressed(sender: UIButton){
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func addButtonPressed(sender: UIButton){
+    
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func subButtonPressed(sender: UIButton){
+        
+        processOperation(operation: .Subtract)
+        
+    }
+    
+    @IBAction func multiplyButtonPressed(sender: UIButton){
+        
+        processOperation(operation: .Multiply)
+    }
+    
+    
+    
+    @IBAction func equalButtonPressed(sender: UIButton){
+        processOperation(operation: currentOperation)
     }
     
     func playSound(){
@@ -36,6 +83,32 @@ class ViewController: UIViewController {
         }
         
         btnSound.play()
+    }
+    
+    func processOperation(operation: Operation){
+        if currentOperation != Operation.Empty{
+            if runningNumebr != ""{
+                rightValStar = runningNumebr;
+                runningNumebr = ""
+                if currentOperation == Operation.Divide{
+                    resultValStar = "\(Double(leftValueStar)! / Double(rightValStar)!)"
+                }else if currentOperation == Operation.Multiply{
+                    resultValStar = "\(Double(leftValueStar)! * Double(rightValStar)!)"
+                }else if currentOperation == Operation.Add{
+                    resultValStar = "\(Double(leftValueStar)! + Double(rightValStar)!)"
+                }else if currentOperation == Operation.Subtract{
+                    resultValStar = "\(Double(leftValueStar)! - Double(rightValStar)!)"
+                }
+                
+                leftValueStar = resultValStar
+                lblOutputTxgt.text = resultValStar
+            }
+            currentOperation = operation
+        }else{
+            leftValueStar = runningNumebr
+            runningNumebr = ""
+            currentOperation = operation
+        }
     }
 }
 
